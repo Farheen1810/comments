eb5d0fdaca3e4b6f0585da7c6cacb4d0e460da23
Farheen1810 <90109907+Farheen1810@users.noreply.github.com>
2021-12-24 23:02:11 +0530

    Add files via upload

---
+++ comment.java
@@ -0,0 +1,16 @@
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("single-line comment");
+        /* This is single-line comment*/
+        System.out.println("multi-line comment");
+        /* This is multi-line comment
+           in java*/
+        System.out.println("documentation comments");
+        /** This is documentation comment
+         * @author JAMES
+         * @version 2.2
+         * @since 2019-03-21
+         */
+    }
+    
+}
